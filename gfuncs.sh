933da2c7c7c13d6fa7f15a118ba58ae5a7ddb1bb
JINGYU GONG <gongjingyu@JINGYUs-MacBook-Pro.local>
2019-09-10 15:58:19 +0800

    update gfuncs.sh

---
+++ gfuncs.sh
@@ -0,0 +1,9 @@
+function texmake {
+    filename=$1
+    pdflatex -synctex=1 -interaction=nonstopmode $filename.tex
+    bibtex $filename
+    pdflatex -synctex=1 -interaction=nonstopmode $filename.tex
+    pdflatex -synctex=1 -interaction=nonstopmode $filename.tex
+    rm $filename.bbl $filename.aux $filename.blg
+    rm $filename.log $filename.synctex.gz $filename.brf $filename.out
+}
