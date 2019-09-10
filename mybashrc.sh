8ec271a551e09584c9572f60549c54891c51fe0c
gongjingyu <gongjingyu@sjtu.edu.cn>
2019-09-10 15:57:46 +0800

    update gfuncs.sh

---
+++ mybashrc.sh
@@ -0,0 +1,10 @@
+#set shell option
+set -o vi
+#set alias
+alias d='dirs -v'
+alias grep='grep -E'
+alias ls='ls -FG'
+alias pd='pushd'
+alias ssh-hcp='ssh -p 20500 gongjingyu@202.120.38.4'
+alias ssh-deb='ssh root@8.9.3.179'
+alias ssh-astro='ssh gjyu@202.120.13.67'
