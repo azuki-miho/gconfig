f552466a4ffbe27aa2df83bee8c5f66699254098
gongjingyu <gongjingyu@sjtu.edu.cn>
2019-09-24 20:05:58 +0800

    change the texmake function

--- gfuncs.sh
+++ gfuncs.sh
@@ -6,6 +6,7 @@ function texmake {
     pdflatex -synctex=1 -interaction=nonstopmode $filename.tex
     rm $filename.bbl $filename.aux $filename.blg
     rm $filename.log $filename.synctex.gz $filename.brf $filename.out
+    rm $filename.nav $filename.snm $filename.toc
 }
 
 function start_vpn {
